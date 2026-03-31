7a9de53d8699dea1787645b853c258e8ea311df6
Themistocles-21 <deriklittle02@gmail.com>
2026-03-30 21:25:25 -0800

    Fixer Up 10

--- src/main/java/com/cscd488/html/SpringbootHtmlApplication.java
+++ src/main/java/com/cscd488/html/SpringbootHtmlApplication.java
@@ -2,11 +2,11 @@ package com.cscd488.html;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
-
 @SpringBootApplication
-
+@Controller
 public class SpringbootHtmlApplication {
 
     public static void main(String[] args) {
