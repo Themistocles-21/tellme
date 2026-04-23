13481db0af28903a43686fbaa856350c65b0d4f2
Themistocles-21 <deriklittle02@gmail.com>
2026-04-22 18:45:41 -0800

    Refactor

--- src/main/java/com/cscd488/html/controller/CustomerController.java
+++ src/main/java/com/cscd488/html/controller/CustomerController.java
@@ -1,17 +1,20 @@
 package com.cscd488.html.controller;
 
 import com.cscd488.html.model.Customer;
+import com.cscd488.html.model.Vehicle;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 import java.util.Locale;
 
 @Controller
 public class CustomerController {
 
     @GetMapping("/home")
-    public String home() {
+    public String home(@RequestParam(required = false) String lang, Model model) {
+        if (lang != null) {
+            model.addAttribute("selectedLang", lang);
+        }
         return "customerInfo";
     }
 
@@ -19,38 +22,9 @@ public class CustomerController {
     public String registerCustomer(@ModelAttribute Customer customer,
                                    @RequestParam String language,
                                    Model model) {
-
-        // Store language in customer object
         customer.setLanguage(language);
-
-        // Set locale for internationalization
-        Locale locale;
-        switch(language) {
-            case "fa":
-                locale = new Locale("fa");
-                break;
-            case "ru":
-                locale = new Locale("ru");
-                break;
-            default:
-                locale = Locale.ENGLISH;
-        }
-
         model.addAttribute("customer", customer);
-        model.addAttribute("vehicle", new com.cscd488.html.model.Vehicle());
-        model.addAttribute("locale", locale);
-
+        model.addAttribute("vehicle", new Vehicle());
         return "vehicleInfo";
-    }
-
-    private Locale getLocaleFromCustomer(Customer customer) {
-        if (customer == null || customer.getLanguage() == null) {
-            return Locale.ENGLISH;
-        }
-        switch(customer.getLanguage()) {
-            case "fa": return new Locale("fa");
-            case "ru": return new Locale("ru");
-            default: return Locale.ENGLISH;
-        }
     }
 }
